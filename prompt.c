b4462be31db470968a944fed9955854f45548c39
nisrine21 <nisrinezaida@gmail.com>
2023-07-15 11:22:06 -0700

    task 1 done

---
+++ prompt.c
@@ -0,0 +1,54 @@
+#include "shell.h"
+#include <sys/wait.h>
+
+/**
+ * prompt - handles the user interaction, command execution
+ * and basic shell functionalities
+ *
+ * @av: array of strings
+ * @nv: array of strings
+ * Return: not return any value
+ */
+
+void prompt(char **av, char **nv)
+{
+	char *string = NULL;
+	int i, status;
+	size_t n = 0;
+	ssize_t n_char;
+	char *argv[] = {NULL, NULL};
+	pid_t ch_pid;
+
+	while (1)
+	{  /*display the prompt*/
+		printf("cisfun$ ");
+		/*read user's cmd*/
+		n_char = getline(&string, &n, stdin);
+		if (n_char == -1)
+		{
+			free(string);
+			exit(EXIT_FAILURE);
+		}
+		i = 0;
+		while (string[i])
+		{
+			if (string[i] == '\n')
+				string[i] = 0;
+			i++;
+		}
+		argv[0] = string;
+		ch_pid = fork();
+		if (ch_pid == -1)
+		{
+			free(string);
+			exit(EXIT_FAILURE);
+		}
+		if (ch_pid == 0)
+		{
+			if (execve(argv[0], argv, nv) == -1)
+				printf("%s: No such file or directory\n", av[0]);
+		}
+		else
+			wait(&status);
+	}
+}
---
+++ shell.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * main - entry point of the simple shell program
+ *
+ * @x: number of command-line arguments
+ * @av: array of strings
+ * @nv: array of strings
+ *
+ * Return: 0 on success
+ */
+
+int main(int x, char **av, char **nv)
+{
+	if (x == 1)
+		prompt(av, nv);
+	return (0);
+}
