94b8030492c38b7912e9658bb4b9aa39bc859232
nisrine21 <nisrinezaida@gmail.com>
2023-07-17 07:52:36 -0700

    updated

--- shell.h
+++ shell.h
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define MAX_COMMAND_OF_NIYOU 64
+#define DELIMITERS " \t\r\n\a"
+
+extern char **environ;
 
 void prompt(char **av, char **env);
 
